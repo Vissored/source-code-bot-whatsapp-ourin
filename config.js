0bc605d49fae67746da6e3ce107ac4457be184fe
Kyo 86 <zhyzxzice@gmail.com>
2026-08-08 19:35:30 +0700

    config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const config = {
 
   session: {
     pairingNumber: "6287871743919", // Nomor WA yang akan di-pair
-    usePairingCode: true, // true = Pairing Code, false = QR Code
+    usePairingCode: false, // true = Pairing Code, false = QR Code
   },
 
   bot: {
